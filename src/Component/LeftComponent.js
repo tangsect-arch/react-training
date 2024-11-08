820c907f3426b4971cd2330885772ba61b808aee
sabari.pn <c-sPraharshan@ihyphen.com>
2024-11-08 11:37:19 +0530

    dyanamic string

--- src/Component/LeftComponent.js
+++ src/Component/LeftComponent.js
@@ -1,54 +1,26 @@
 import "./LeftComponent.css";
-
+// import { generateRandomArray } from "../Helpers/utils";
 const title = [
-  {
-    id: 1,
-    title: "ToDo 1",
-  },
-  {
-    id: 2,
-    title: "ToDo 2",
-  },
-  {
-    id: 3,
-    title: "ToDo 3",
-  },
-  {
-    id: 4,
-    title: "ToDo 4",
-  },
-  {
-    id: 5,
-    title: "ToDo 5",
-  },
-  {
-    id: 6,
-    title: "ToDo 6",
-  },
-  {
-    id: 7,
-    title: "ToDo 7",
-  },
-  {
-    id: 8,
-    title: "ToDo 8",
-  },
-  {
-    id: 9,
-    title: "ToDo 9",
-  },
-  {
-    id: 10,
-    title: "ToDo 10",
-  },
+  "Work",
+  "Personal",
+  "Fitness",
+  "Shopping",
+  "Study",
+  "Chores",
+  "Entertainment",
 ];
 
 export default function LeftComponent() {
   function toDo() {
-    return title.map((item) => {
+    // const title = generateRandomArray(5);
+    const random = Math.floor(Math.random() * title.length);
+    return title.map((item, index) => {
       return (
-        <div key={item.id} className="todo-item">
-          {item.title}
+        <div
+          key={index}
+          className={`todo-item ${index === random ? "selected" : ""}`}
+        >
+          {item}
         </div>
       );
     });
