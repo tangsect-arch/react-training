535195e8ecf6ec5ae1e13806e9067982a2ce2dff
sabari.pn <c-sPraharshan@ihyphen.com>
2024-11-12 12:42:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { ComponentA } from "./component/ComponentA";
+import { ComponentB } from "./component/ComponentB";
 
 function App() {
-  return <div className="container">Hello World</div>;
+  return (
+    <>
+      <h1>Parent</h1>
+      <ComponentA />
+      <ComponentB />
+    </>
+  );
 }
 
 export default App;
