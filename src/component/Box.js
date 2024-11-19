dba6e951a82b9f9e7dedd014710ed4a4f57ee8a6
sabari.pn <c-sPraharshan@ihyphen.com>
2024-11-19 12:47:13 +0530

    Day 07

---
+++ src/component/Box.js
@@ -0,0 +1,19 @@
+import Item from "./Item";
+
+import { Component } from "react";
+
+class Box extends Component {
+  render() {
+    return (
+      <>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </>
+    );
+  }
+}
+
+export default Box;
---
+++ src/component/Item.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Item extends Component {
+  render() {
+    return <h3>Item</h3>;
+  }
+}
+
+export default Item;
