bfc7e62e6bdc5f068b759a0e587b5772bccceab3
sabari.pn <c-sPraharshan@ihyphen.com>
2024-11-19 12:33:14 +0530

    Day 07

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Component } from "react";
 import "./App.css";
+import Box from "./component/Box";
 
-function App() {
-  return <div className="container">Hello World</div>;
+// function App() {
+//   return <div className="container">Hello World</div>;
+// }
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="container">Hello World</div>
+        <Box />
+      </>
+    );
+  }
 }
 
-export default App;
+// export default App;
