5b212355480d6d4a3940760d48cf79c23d2f7d50
sabari.pn <c-sPraharshan@ihyphen.com>
2024-11-28 11:53:53 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
+
+const ComponentA = lazy(() => import("./ComponentA.js"));
+const ComponentB = lazy(() => import("./ComponentB.js"));
 
 function App() {
-  return <div className="container">Hello World</div>;
+  return (
+    <>
+      <Suspense fallback={<div>loading</div>}>
+        <ComponentA />
+        <ComponentB />
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
---
+++ src/component/ComponentB.js
@@ -0,0 +1,5 @@
+import React, { useRef } from "react";
+
+export default ComponentB = () => {
+  return <div>ComponentB</div>;
+};
